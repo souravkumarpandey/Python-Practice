f157c112153895d82b70cfb35a9e7e2459f00fd5
sourav kumar pandey <soukumpan@gmail.com>
2024-12-20 18:15:20 +0530

    greatest of 3 number

---
+++ greatest_of_3.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    num1=int(input("Enter the first"))
+    num2 = int(input("Enter the second no."))
+    num3= int(input("enter the third no"))
+
+    if num1>num2:
+        print("Greatest of three no ", num1)
+    elif num2>num3:
+        print("Greatest of three no ", num2)
+    else:
+        print("Greatest of three no ", num3)
